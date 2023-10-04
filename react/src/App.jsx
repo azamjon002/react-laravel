253f21471cf22e6d13a6eaa4df7d378c0f7ae29d
Azamjon Qochqorov <azamjonqochqorov02@gmail.com>
2023-10-04 16:41:52 +0500

    all of the needed packages are installed

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import {BeakerIcon, UserIcon} from '@heroicons/react/24/solid'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-amber-300">
+        App.jsx
+      <UserIcon className={'w-5'}/>
+    </div>
+  )
+}
+
+export default App
