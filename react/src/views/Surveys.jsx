30bfa20f2cf6c20d6f6f01d47c836ab9a48987ef
Azamjon Qochqorov <azamjonqochqorov02@gmail.com>
2023-10-15 21:43:48 +0500

    test

---
+++ react/src/views/Surveys.jsx
@@ -0,0 +1,9 @@
+import PageComponent from "../components/PageComponent.jsx";
+
+export default function Surveys(){
+  return (
+    <PageComponent title="Surveys">
+      Surveys Content
+    </PageComponent>
+  )
+}
